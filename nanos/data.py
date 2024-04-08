e57ddd31c6cbe894dd30e6b2759d52747d2424e9
Alex <aleosd@gmail.com>
2024-04-08 15:25:23 +0100

    [remove-empty] Adds function to remove empty items from list or dict

--- nanos/data.py
+++ nanos/data.py
@@ -1,9 +1,11 @@
 import typing as t
 from collections.abc import Iterable, Sequence
+from functools import partial
 
 T = t.TypeVar("T")
 K = t.TypeVar("K")
 DEFAULT_ID_ATTR_NAME: t.Final = "id"
+EMPTY_VALUES: t.Final[list[t.Any]] = ["", None, [], {}]
 
 
 def chunker(seq: Sequence[T], size: int) -> list[Sequence[T]]:
@@ -39,3 +41,33 @@ def idfy(
     if hasattr(obj, id_field_name):
         return {getattr(obj, id_field_name): obj}
     raise ValueError(f"Can't get {id_field_name} attribute from {obj}")
+
+
+def remove_empty_members(obj: T, empty: list[t.Any] | None = None) -> T | None:
+    empty = empty or EMPTY_VALUES
+    cleaners: dict[type, t.Callable[[t.Any, list[t.Any]], t.Any]]
+    cleaners = {
+        dict: _remove_empty_members_from_dict,
+        list: _remove_empty_members_from_list,
+    }
+    if (cleaner := cleaners.get(type(obj))) is not None:
+        obj = cleaner(obj, empty)
+    if obj in empty:
+        return None
+    return obj
+
+
+def _remove_empty_members_from_dict(
+    obj: dict[t.Any, t.Any], empty: list[t.Any]
+) -> dict[t.Any, t.Any]:
+    for key in list(obj.keys()):
+        obj[key] = remove_empty_members(obj[key], empty)
+        if obj[key] is None:
+            del obj[key]
+    return obj
+
+
+def _remove_empty_members_from_list(obj: list[t.Any], empty: list[t.Any]) -> list[t.Any]:
+    return list(
+        filter(lambda x: x not in empty, map(partial(remove_empty_members, empty=empty), obj))
+    )
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,7 @@
 from unittest import mock
 
 import pytest
+from hamcrest import assert_that, equal_to
 
 import nanos
 
@@ -20,3 +21,25 @@ DUMMY_OBJ.id = 42
 )
 def test_idfy(data, expected_idfy):
     assert nanos.data.idfy(data) == expected_idfy
+
+
+@pytest.mark.parametrize(
+    "input_obj, expected_output",
+    [
+        ([], None),
+        (0, 0),
+        (1, 1),
+        ((), ()),
+        ("", None),
+        ({}, None),
+        (["a"], ["a"]),
+        ([""], None),
+        (["", None, [], {}], None),
+        (["foo", ""], ["foo"]),
+        ({"foo": "bar"}, {"foo": "bar"}),
+        ({"foo": ""}, None),
+        ({"foo": "", "bar": [1, 2, ""], "baz": ["", ""]}, {"bar": [1, 2]}),
+    ],
+)
+def test__remove_empty_members__default_empty(input_obj, expected_output):
+    assert_that(nanos.data.remove_empty_members(input_obj), equal_to(expected_output))
