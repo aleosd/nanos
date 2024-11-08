0f5a3bb52792fb400f13c08dbb0acfdd8e5804a4
Alex <aleosd@gmail.com>
2024-11-08 12:11:19 +0000

    [more-dt] Adds more db utils

--- nanos/dt.py
+++ nanos/dt.py
@@ -2,23 +2,35 @@ import datetime
 
 
 def days_after_now(
-    days_num: int = 1, tz: datetime.tzinfo = datetime.timezone.utc
+    days_num: int = 1, tz: datetime.tzinfo | None = datetime.timezone.utc
 ) -> datetime.datetime:
     return datetime.datetime.now(tz=tz) + datetime.timedelta(days=days_num)
 
 
-def tomorrow(tz: datetime.tzinfo = datetime.timezone.utc) -> datetime.datetime:
+def tomorrow(tz: datetime.tzinfo | None = datetime.timezone.utc) -> datetime.datetime:
     return days_after_now(tz=tz)
 
 
 def days_before_now(
-    days_num: int = 1, tz: datetime.tzinfo = datetime.timezone.utc
+    days_num: int = 1, tz: datetime.tzinfo | None = datetime.timezone.utc
 ) -> datetime.datetime:
     return days_after_now(-days_num, tz=tz)
 
 
-def yesterday(tz: datetime.tzinfo = datetime.timezone.utc) -> datetime.datetime:
+def yesterday(tz: datetime.tzinfo | None = datetime.timezone.utc) -> datetime.datetime:
     return days_before_now(tz=tz)
+
+
+def yesterday_start(tz: datetime.tzinfo | None = datetime.timezone.utc) -> datetime.datetime:
+    return datetime.datetime.combine(yesterday(tz=tz), datetime.datetime.min.time()).replace(
+        tzinfo=tz
+    )
+
+
+def yesterday_end(tz: datetime.tzinfo | None = datetime.timezone.utc) -> datetime.datetime:
+    return datetime.datetime.combine(yesterday(tz=tz), datetime.datetime.max.time()).replace(
+        tzinfo=tz
+    )
 
 
 def today_eod(tz: datetime.tzinfo = datetime.timezone.utc) -> datetime.datetime:
