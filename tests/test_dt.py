b6cf8f2fad305e6fe46522cf182020e0456287c4
Alex <aleosd@gmail.com>
2024-11-08 12:14:56 +0000

    [more-dt] Adds more db utils (#10)

--- tests/test_dt.py
+++ tests/test_dt.py
@@ -83,3 +83,50 @@ def test__days_after_now__default_args(mocked_datetime):
     # then
     assert_that(result, equal_to(expected_result))
     mocked_datetime.datetime.now.assert_called_once_with(tz=datetime.timezone.utc)
+
+
+def test__yesterday_start__default_tz():
+    # arrange
+    yesterday_dt = datetime.datetime(2023, 2, 2, 14, 15, 2, 999888, tzinfo=datetime.timezone.utc)
+    expected_result = datetime.datetime(2023, 2, 2, 0, 0, 0, 0, tzinfo=datetime.timezone.utc)
+
+    # when
+    with mock.patch("nanos.dt.yesterday") as m:
+        m.return_value = yesterday_dt
+        result = dt.yesterday_start()
+
+    # then
+    m.assert_called_once_with(tz=datetime.timezone.utc)
+    assert_that(result, equal_to(expected_result))
+
+
+def test__yesterday_start__no_tz():
+    # arrange
+    yesterday_dt = datetime.datetime(2023, 2, 2, 14, 15, 2, 999888)
+    expected_result = datetime.datetime(2023, 2, 2, 0, 0, 0, 0)
+
+    # when
+    with mock.patch("nanos.dt.yesterday") as m:
+        m.return_value = yesterday_dt
+        result = dt.yesterday_start(tz=None)
+
+    # then
+    m.assert_called_once_with(tz=None)
+    assert_that(result, equal_to(expected_result))
+
+
+def test__yesterday_end():
+    # arrange
+    yesterday_dt = datetime.datetime(2023, 2, 2, 14, 15, 2, 999888, tzinfo=datetime.timezone.utc)
+    expected_result = datetime.datetime(
+        2023, 2, 2, 23, 59, 59, 999999, tzinfo=datetime.timezone.utc
+    )
+
+    # when
+    with mock.patch("nanos.dt.yesterday") as m:
+        m.return_value = yesterday_dt
+        result = dt.yesterday_end()
+
+    # then
+    m.assert_called_once_with(tz=datetime.timezone.utc)
+    assert_that(result, equal_to(expected_result))
